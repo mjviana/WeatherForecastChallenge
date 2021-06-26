5a73d438fc9ba5ffddcf537d93150a61ed220aac
Mário Viana <mj.viana1993@hotmail.com>
2021-06-26 15:05:31 +0100

    Add weather forecast chart for current day

--- app/src/main/java/com/mobiweb/challenge/weatherforecast/ui/fragments/WeatherForecastFragment.kt
+++ app/src/main/java/com/mobiweb/challenge/weatherforecast/ui/fragments/WeatherForecastFragment.kt
@@ -1,5 +1,6 @@
 package com.mobiweb.challenge.weatherforecast.ui.fragments
 
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -11,6 +12,12 @@ import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.mobiweb.challenge.weatherforecast.databinding.FragmentWeatherForecastBinding
 import com.mobiweb.challenge.weatherforecast.ui.adapters.DayWeatherForecastRecyclerViewAdapter
@@ -19,13 +26,16 @@ import com.mobiweb.challenge.weatherforecast.utilities.Resource
 import java.time.LocalDateTime
 import kotlin.math.roundToInt
 
-
 class WeatherForecastFragment : Fragment() {
 
     private lateinit var binding: FragmentWeatherForecastBinding
     private lateinit var weatherForecastViewModel: WeatherForecastViewModel
     private lateinit var weatherForecastRecyclerView: RecyclerView
     private lateinit var weatherForecastAdapter: DayWeatherForecastRecyclerViewAdapter
+    private lateinit var lineChart: LineChart
+    private var lineChartList = ArrayList<Entry>()
+    private lateinit var lineChartDataSet: LineDataSet
+    private lateinit var lineChartData: LineData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,19 +56,25 @@ class WeatherForecastFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setupRecyclerView()
+        lineChart = binding.weatherChart
 
         weatherForecastViewModel.currentWeatherData.observe(viewLifecycleOwner, { response ->
             when (response) {
                 is Resource.Success -> {
+                    hideCurrentTemperatureWeatherForecastProgressBar()
                     response.data?.let { currentWeatherData ->
                         binding.currentTemperatureValue.text =
                             currentWeatherData.main.temp.roundToInt().toString() + "º"
                     }
                 }
                 is Resource.Error -> {
+                    hideCurrentTemperatureWeatherForecastProgressBar()
                     response.message?.let { message ->
                         displayError(message)
                     }
+                }
+                is Resource.Loading -> {
+                    showCurrentTemperatureWeatherForecastProgressBar()
                 }
             }
         })
@@ -66,21 +82,41 @@ class WeatherForecastFragment : Fragment() {
         weatherForecastViewModel.weatherForecast.observe(viewLifecycleOwner, { response ->
             when (response) {
                 is Resource.Success -> {
+                    hideWeatherForecastProgressBar()
                     response.data?.let { weatherForecast ->
                         val details = weatherForecast.details
+
+                        // get the dates in weatherForecast response
                         val dates = mutableListOf<LocalDateTime>()
                         for (detail in details) {
                             dates.add(detail.date)
                         }
                         val days = dates.distinctBy { it.dayOfMonth } as MutableList<LocalDateTime>
                         days.removeAt(0)
+
+                        // Set up the weather details for the current day
+                        val todayDetails =
+                            details.filter { it.date.dayOfYear == LocalDateTime.now().dayOfYear }
+                        todayDetails.forEach {
+                            lineChartList.add(
+                                Entry(
+                                    it.date.hour.toFloat(),
+                                    it.main.temp.roundToInt().toFloat()
+                                )
+                            )
+                        }
+                        setupWeatherChart()
                         weatherForecastAdapter.setData(days, details)
                     }
                 }
                 is Resource.Error -> {
+                    hideWeatherForecastProgressBar()
                     response.message?.let { message ->
                         displayError(message)
                     }
+                }
+                is Resource.Loading -> {
+                    showWeatherForecastProgressBar()
                 }
             }
         })
@@ -95,6 +131,35 @@ class WeatherForecastFragment : Fragment() {
         weatherForecastRecyclerView.setHasFixedSize(true)
     }
 
+    private fun setupWeatherChart() {
+        lineChartDataSet = LineDataSet(lineChartList, "Today's Temperature")
+        lineChartData = LineData(lineChartDataSet)
+
+        lineChart.data = lineChartData
+        lineChartDataSet.color = Color.BLUE
+        lineChartDataSet.valueTextColor = Color.BLUE
+        lineChartDataSet.valueTextSize = 12f
+        val chartDescription = lineChart.description
+        chartDescription.isEnabled = true
+        chartDescription.text = "X -> Time. Y -> Temperature"
+
+        val xAxis: XAxis = lineChart.xAxis
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        xAxis.textSize = 12f
+        xAxis.textColor = Color.BLACK
+        xAxis.setDrawAxisLine(false)
+        xAxis.setDrawGridLines(false)
+
+        val yRightAxis: YAxis = lineChart.axisRight
+        yRightAxis.textSize = 12f
+        yRightAxis.textColor = Color.BLACK
+        yRightAxis.setDrawAxisLine(false)
+        yRightAxis.setDrawGridLines(false)
+
+        val yLeftAxis: YAxis = lineChart.axisLeft
+        yLeftAxis.isEnabled = false
+    }
+
     private fun displayError(message: String) {
         MaterialAlertDialogBuilder(context as FragmentActivity)
             .setTitle("UPS!")
@@ -103,5 +168,25 @@ class WeatherForecastFragment : Fragment() {
             .setPositiveButton("Ok") { dialog, which ->
             }
             .show()
+    }
+
+    private fun showCurrentTemperatureWeatherForecastProgressBar() {
+        binding.progressBarCurrentTemperature.visibility = View.VISIBLE
+        binding.weatherPredictionList.visibility = View.GONE
+    }
+
+    private fun showWeatherForecastProgressBar() {
+        binding.currentTemperatureValue.visibility = View.GONE
+        binding.progressBarWeatherForecast.visibility = View.VISIBLE
+    }
+
+    private fun hideWeatherForecastProgressBar() {
+        binding.currentTemperatureValue.visibility = View.VISIBLE
+        binding.progressBarWeatherForecast.visibility = View.GONE
+    }
+
+    private fun hideCurrentTemperatureWeatherForecastProgressBar() {
+        binding.progressBarCurrentTemperature.visibility = View.GONE
+        binding.weatherPredictionList.visibility = View.VISIBLE
     }
 }
